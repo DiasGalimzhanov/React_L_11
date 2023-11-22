045b9ec2c69c532281d69682741e642582d4a629
Галимжанов Диас Амангельдинович <GalimzhanovD@lcoast.itstep>
2023-11-22 21:55:07 +0600

    finish

--- src/components/ToDoCreater/ToDoCreater.jsx
+++ src/components/ToDoCreater/ToDoCreater.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import s from "./ToDoCreater.module.scss"
 
-function Creater() {
+function Creater({ setTodos }) {
+    const [title, setTitle] = useState("");
+    const [task, setTask] = useState("");
+
+    const onChangeTitle = (e) => setTitle(e.target.value);
+    const onChangeTask = (e) => setTask(e.target.value);
+
+    const onClick = () => {
+        const newTodo = {
+            title,
+            task,
+            date: Date.now(),
+        };
+
+        setTodos(prev=> [...prev,newTodo])
+    }
+
     return <div className={s.creater}>
-        <input type="text" className={s.imput} placeholder="Заголовок"></input>
-        <input type="text" className={s.imput} placeholder="Ваша Заметка"></input>
-        <Button className={s.button}>Save</Button>
+        <input type="text" className={s.imput} placeholder="Заголовок" onChange={onChangeTitle}></input>
+        <input type="text" className={s.imput} placeholder="Ваша Заметка" onChange={onChangeTask}></input>
+        <Button className={s.button} onClick={onClick} >Save</Button>
     </div>
 }
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../components/Button";
 import s from "./HomePage.module.scss";
 import Header from "../../components/Header";
 import Creater from "../../components/ToDoCreater/ToDoCreater";
 import TodoGrid from "../../components/TodoGrid/TodoGrid";
 
+
 function HomePage() {
-    const todos = [{
+
+
+    const [todos, setTodos] = useState ([{
         title: "Task 1",
         task: "Do somthing",
         date: "21.11.2023",
@@ -18,15 +21,11 @@ function HomePage() {
         title: "Task 3",
         task: "Do somthing",
         date: "21.11.2023",
-    }];
-
-    const onButtonClick = () => {
-        console.log("smt");
-    }
+    }]);
 
     return (<div>
         <Header></Header>
-        <Creater></Creater>
+        <Creater setTodos={setTodos}></Creater>
         <TodoGrid todos={todos}></TodoGrid>
     </div>
     )
